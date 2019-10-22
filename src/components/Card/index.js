fa0399783c16c6a1be9ab0fc9177b77309ffd35f
Willian Monteiro <willian@zhf.com.br>
2019-10-21 23:03:56 -0200

    estilos para os cards e lista

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Recriar tela do PIPEFY usando REACT</p>
+      <img src="https://avatars3.githubusercontent.com/u/11152984" alt="" />
+    </Container>
+  );
+}
---
+++ src/components/List/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 0 15px;
+  height: 100%;
+  flex: 0 0 320px;
+
+  /* 
+    flex-grow: 0; Possibilidade de Esticar
+    flex-shrink: 0; Possibilidade de Reduzir
+    flex-basis: 320px; Tamanho base
+  */
+
+  & + div {
+    border-left: 1px solid rgba(0, 0, 0, 0.3);
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 45px;
+
+    h2 {
+      font-weight: 500;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+
+    button {
+      width: 42px;
+      height: 42px;
+      border-radius: 18px;
+      background: #3b5bfd;
+      border: 0;
+      cursor: pointer;
+    }
+  }
+
+  ul {
+    margin-top: 30px;
+  }
+`;
